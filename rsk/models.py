aaf3026005b76261ce89c1f46bd09ade3cd2714d
unknown <karthikeyan1296@yahoo.com>
2020-09-08 11:15:16 +0530

    registering table in admin.py and adding data using admin

--- rsk/models.py
+++ rsk/models.py
@@ -5,22 +5,22 @@ from django.db import models
 class Topic(models.Model):
     topic_name=models.CharField(max_length=30,unique=True,blank=False)
 
-    #def __str__(self):
-        #return self.topic_name
+    def __str__(self):
+        return self.topic_name
 
 class Webpage(models.Model):
     topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100,unique=True,blank=False)
     url=models.URLField(max_length=100,unique=True,blank=False)
 
-    #def __str__(self):
-        #return self.name
+    def __str__(self):
+        return self.name
 
 class AccesDetails(models.Model):
     webpage=models.ForeignKey(Webpage,on_delete=models.CASCADE)
-    datetime=models.DateField(auto_now_add=True)
+    datetime=models.DateTimeField()
 
-    #def __str__(self):
-        #return str(self.datetime).split(" ")[0]
+    def __str__(self):
+        return str(self.datetime).split(" ")[0]
     
     
