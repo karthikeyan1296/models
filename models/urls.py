00ca6c96566abfbcea1b779ae60968e9251300bc
unknown <karthikeyan1296@yahoo.com>
2020-09-10 13:53:32 +0530

    adding data by user

--- models/urls.py
+++ models/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rsk import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add_topic/',views.create_topic,name="add_topic"),
+    path('add_webpage/',views.create_webpage,name="add_webpage")
 ]
--- rsk/models.py
+++ rsk/models.py
@@ -1,16 +1,20 @@
 from django.db import models
-
-# Create your models here.
+from django.core import validators
+from django.core.exceptions import ValidationError
 
 class Topic(models.Model):
-    topic_name=models.CharField(max_length=30,unique=True,blank=False)
+    topic_name=models.CharField(max_length=30,unique=True,blank=False,validators=[validators.MaxLengthValidator(10)])
 
     def __str__(self):
         return self.topic_name
 
+def validate_name(name):
+    if not name.isalpha():
+        raise ValidationError("{} is having characters other than alphabets".format(name))
+
 class Webpage(models.Model):
     topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
-    name=models.CharField(max_length=100,unique=True,blank=False)
+    name=models.CharField(max_length=100,unique=True,validators=[validate_name],blank=False)
     url=models.URLField(max_length=100,unique=True,blank=False)
 
     def __str__(self):
